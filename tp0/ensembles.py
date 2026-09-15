58f97ce4e45140b57249fb7a757ffafc7fe4ae1f
Baptiste <ma.baptiste85@gmail.com>
2026-09-15 15:30:36 +0200

    question2 exo4

--- tp0/ensembles.py
+++ tp0/ensembles.py
@@ -1,4 +1,5 @@
 
+#Question1
 def robots_double_mission(robots_exploration, robots_transport):
     return (robots_exploration & robots_transport)
 
@@ -9,6 +10,19 @@ def robots_exploration_seulement(robots_exploration, robots_transport):
     return (robots_exploration - robots_transport)
 
 
+#Question2
+def ajouter_robot_mission(rob, val):
+    ensemble = rob
+    ensemble.add(val)
+    return ensemble
+
+
+def retirer_robot_mission(rob, val):
+    ensemble = set()
+    for i in rob:
+        if i!=val:
+            ensemble.add(i)
+    return ensemble
 
 robots_exploration = {"R2", "R5", "R7"}
 robots_transport = {"R5", "R9", "R7", "R3"}
@@ -20,3 +34,11 @@ exploration_seule = robots_exploration_seulement(robots_exploration, robots_tran
 assert double_mission == {"R5", "R7"}
 assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
 assert exploration_seule == {"R2"}
+
+
+ajout = ajouter_robot_mission(robots_exploration, "R8")
+retrait = retirer_robot_mission(robots_transport, "R9")
+assert ajout == {"R2", "R5", "R7", "R8"}
+assert retrait == {"R3", "R5", "R7"}
+# L’ensemble d’origine ne doit pas avoir été modifié
+assert robots_transport == {"R5", "R9", "R7", "R3"}
