261f587b4a27c9302fbd5968506bf0324c53c56c
Baptiste <ma.baptiste85@gmail.com>
2026-09-15 14:51:45 +0200

    Question 1 exo3

---
+++ tp0/exercice3.py
@@ -0,0 +1,15 @@
+
+#Question1
+def afficher_releve(capteur):
+    return f"Capteur {capteur[0]} : {capteur[1]} {capteur[2]}"
+
+
+releve1 = ("laser_avant", 2.35, "m")
+releve2 = ("laser_arriere", 1.10, "m")
+releve3 = ("gyroscope", 87.5, "deg")
+releves = [releve1, releve2, releve3]
+
+
+assert len(releves) == 3
+assert releves[0][0] == "laser_avant"
+assert afficher_releve(releve1) == "Capteur laser_avant : 2.35 m"
