96466ad1f0c54ee77de7e1ed8dcf3a4a892089c1
Baptiste <ma.baptiste85@gmail.com>
2026-09-22 14:17:30 +0200

    Exercice 3 - Question 2

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -1,22 +1,24 @@
 """Exercice 3 - Classe Habitant"""
 class Habitant:
+    """Classe Habitant reprenant son adresse, son age, son nom et ses animaux"""
     def __init__(self, nom, age, adresse, animaux=None):
         self.nom = nom
         self.age = age
         self.adresse = adresse
-        if animaux != None:
+        if animaux is not None:
             self.animaux = animaux
         else:
             self.animaux = {}
 
     def affichage_adresse(self):
-        return f"{self.nom} habite a {self.adresse}"
+        """Affiche l'adresse de l'habitant dans le terminal"""
+        print(f"{self.nom} habite a {self.adresse}")
 
     def compte_animal(self,animal):
+        """Renvoie le nombre de "Animal" que l'habitant possede"""
         if animal in self.animaux:
             return self.animaux[animal]
-        else:
-            return 0
+        return 0
 
 
 h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
