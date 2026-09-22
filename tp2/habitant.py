aca16c7f55aa45f54753a4aeef039034804e5365
Baptiste <ma.baptiste85@gmail.com>
2026-09-22 15:40:14 +0200

    Exercice 7 - Question 1

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -1,8 +1,10 @@
 """Exercice 3 - Classe Habitant & Exercice 4 - Encapsulation"""
 from multipledispatch import dispatch
+from abc import ABC,abstractmethod
 
 
-class Habitant:
+
+class Habitant(ABC):
     """Classe Habitant reprenant son adresse, son age, son nom et ses animaux"""
     def __init__(self, nom, age, adresse, animaux=None):
         self.__nom = nom
@@ -61,6 +63,15 @@ class Habitant:
             return self.__animaux[animal]
         return 0
 
+    #Exercice 7
+    @abstractmethod
+    def calcul_nombre_annee_avant_retraite(self):
+        age_retraite = 62
+        if self.age >= age_retraite:
+            return "Déjà à la retraite"
+        else:
+            return age_retraite - self.age
+
 #Exercice 6
 @dispatch(object,str)
 def set_info(habitant,nom):
@@ -74,15 +85,16 @@ def set_info(habitant,nom,age):
     habitant.set_age(age)
 
 
-h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
-""" TESTS EXERCICE 3
+#h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+""" 
+#TESTS EXERCICE 3
 
 assert h1.nom == "Aldric"
 assert h1.compte_animal("vaches") == 3
 assert h1.compte_animal("moutons") == 0
 h1.affichage_adresse() # affiche "Aldric habite a Rue A"
-"""
 
+#TESTS EXERCICE 4
 h1.age = 26
 assert h1.age == 26
 try:
@@ -93,8 +105,9 @@ except ValueError:
 
 
 
-#Exercice 6
+#TESTS EXERCICE 6
 
 h2 = Habitant("Bob", 40, "Rue C")
 set_info(h2, "Robert") # met a jour le nom seulement
 set_info(h2, "Robert", 41) # met a jour le nom et l’age
+"""
